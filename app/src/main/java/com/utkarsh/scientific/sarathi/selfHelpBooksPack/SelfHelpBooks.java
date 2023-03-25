6c7e347197863c11941659ca745dfd489c502dfe
UTKARSH <utkarshtripathi422@gmail.com>
2023-03-25 22:18:09 +0530

    Game Integration

--- app/src/main/java/com/utkarsh/scientific/sarathi/selfHelpBooksPack/SelfHelpBooks.java
+++ app/src/main/java/com/utkarsh/scientific/sarathi/selfHelpBooksPack/SelfHelpBooks.java
@@ -5,12 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.ContentInfo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
