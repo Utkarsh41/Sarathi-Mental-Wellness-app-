74a2e6db63f22bf17406b70a39a1c234b6fc1c5c
UTKARSH <utkarshtripathi422@gmail.com>
2023-04-01 01:32:33 +0530

    Dependency Fixes and Optimization

--- app/src/main/java/com/utkarsh/scientific/sarathi/selfHelpBooksPack/ItemClickListerner.java
+++
@@ -1,8 +0,0 @@
-package com.utkarsh.scientific.sarathi.selfHelpBooksPack;
-
-
-import android.view.View;
-
-public interface ItemClickListerner {
-    void onClick(View view, int position, boolean isLongClick);
-}
